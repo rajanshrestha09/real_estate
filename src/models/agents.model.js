0c00d416dc38ab8b46a7f047dbe32d525ec79bbd
Rajan Shrestha <rajan.shr09@gmail.com>
2024-01-08 22:09:03 -0700

    Agents model

---
+++ src/models/agents.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const agentSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            validate: {
+                validator: function (value) {
+                    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[A-Za-z!@#$%^&*()_+]{8,}$/.test(value)
+                },
+                message: props => `${props.value} is not valid password`
+            },
+            required: [true, "Password required"]
+        },
+        phone: {
+            type: String,
+            validate: {
+                validator: function (v) {
+                    return /\d{3}-\d{3}-\d{4}/.test(v);
+                },
+                message: props => `${props.value} is not a valid phone number!`
+            },
+            required: [true, 'User phone number required']
+        },
+        licenseNumber: {
+            type: String,
+            requied: [true, "License number is required"]
+        },
+        profileImage: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Agent = mongoose.model("Agent", agentSchema)
