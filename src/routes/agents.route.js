fad7a51b2699c5292dcd49045232cb6247faa414
Rajan Shrestha <rajan.shr09@gmail.com>
2024-01-08 22:59:13 -0700

    Inject multer middleware

--- src/routes/agents.route.js
+++ src/routes/agents.route.js
@@ -1,11 +1,20 @@
-import {Router} from "express"
-
+import { Router } from "express"
 import { registerAgent } from "../controllers/agents.controller.js"
+import { upload } from "../middlewares/multer.middleware.js"
 
 
 const router = Router();
 
-router.route("/register").post(registerAgent)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "profileImage",
+            maxCount: 1
+        }
+    ]
+    )
+    , registerAgent)
+
 
 
 export default router
