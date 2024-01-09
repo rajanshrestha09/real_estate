a62886b7287de55f0bda51a5c530cd1f56632569
Rajan Shrestha <rajan.shr09@gmail.com>
2024-01-08 22:42:35 -0700

    Route to register Agent

--- src/app.js
+++ src/app.js
@@ -11,10 +11,15 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true}, limit="16kb"))
+app.use(express.urlencoded({extended:true}, {limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// Routes
+import registerAgent from "./routes/agents.route.js";
+
+app.use("/api/v1/agent", registerAgent)
 
 
 export {app}
